6152b0d0f7747ef00f7eba8cfa1ff6f192d91858
Moiz <66357790+moiz-r@users.noreply.github.com>
2026-07-17 02:05:06 +0500

    Add: minimize-the-maximum-difference-of-pairs - submission-2

---
+++ Data Structures & Algorithms/minimize-the-maximum-difference-of-pairs/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minimizeMax(self, nums: List[int], p: int) -> int:
+        ans = []
+        nums.sort()
+        len_ = len(nums)
+        l = 0 
+        r = nums[-1] - nums[0]
+
+        def canPair(threshold):
+            count = 0
+            i = 0
+            while i < len_ - 1:
+                diff = nums[i+1] - nums[i]
+
+                if diff <= threshold:
+                    count += 1
+                    i += 2
+                    if count == p:
+                        return True
+                else:
+                    i += 1
+            
+            return count >= p
+        
+        while l <= r:
+            mid = (l +r) // 2
+            
+            if canPair(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+            
+        return l
+
+
